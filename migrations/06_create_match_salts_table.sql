6f7256dc202f982cdb0baca3f55c3f90f77615dc
Manuel Raimann <raimannma@outlook.de>
2024-10-16 03:13:15 +0200

    Format sql files

--- migrations/06_create_match_salts_table.sql
+++ migrations/06_create_match_salts_table.sql
@@ -1,7 +1,7 @@
 CREATE TABLE IF NOT EXISTS match_salts
 (
- match_id      UInt64,
- cluster_id    UInt32,
- metadata_salt UInt32,
- replay_salt   UInt32
+    match_id UInt64,
+    cluster_id UInt32,
+    metadata_salt UInt32,
+    replay_salt UInt32
 ) ENGINE = ReplacingMergeTree ORDER BY (match_id);
