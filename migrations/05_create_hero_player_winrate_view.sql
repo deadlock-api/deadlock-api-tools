04e6f2d462b5d019d4375355a3f2809704c5714f
Manuel Raimann <raimannma@outlook.de>
2024-10-26 21:50:18 +0200

    Adjust hero_player_winrate to contain match metadata data

--- migrations/05_create_hero_player_winrate_view.sql
+++ migrations/05_create_hero_player_winrate_view.sql
@@ -3,9 +3,20 @@ CREATE MATERIALIZED VIEW hero_player_winrate
 REFRESH EVERY 10 MINUTES
 ENGINE = MergeTree() ORDER BY (hero_id, account_id)
 POPULATE AS
-SELECT `players.hero_id` as hero_id, `players.account_id` as account_id, SUM(winner == `players.team`) AS wins, COUNT() AS total
-FROM finished_matches
-      ARRAY JOIN players
+WITH match_hero_account_win AS (
+ SELECT hero_id, account_id, mi.winning_team == team AS win
+ FROM match_player
+       INNER JOIN match_info mi USING (match_id)
+
+ UNION ALL
+
+ SELECT `players.hero_id` as hero_id, `players.account_id` as account_id, winner == `players.team` AS win
+ FROM finished_matches
+       ARRAY JOIN players
+ WHERE match_id NOT IN (SELECT match_id FROM match_info)
+)
+SELECT hero_id, account_id, SUM(win) AS wins, COUNT() AS total
+FROM match_hero_account_win
 GROUP BY hero_id, account_id
 HAVING total >= 10
  SETTINGS allow_experimental_refreshable_materialized_view = 1;
---
+++ migrations/12_create_player_view.sql
@@ -0,0 +1,13 @@
+DROP VIEW IF EXISTS player;
+CREATE MATERIALIZED VIEW player
+REFRESH EVERY 10 MINUTES
+ENGINE = MergeTree() ORDER BY account_id
+POPULATE AS
+SELECT DISTINCT account_id
+FROM match_player
+UNION DISTINCT
+SELECT DISTINCT players.account_id
+FROM finished_matches
+      ARRAY JOIN players
+ ORDER BY `players.account_id`
+ SETTINGS allow_experimental_refreshable_materialized_view = 1;
