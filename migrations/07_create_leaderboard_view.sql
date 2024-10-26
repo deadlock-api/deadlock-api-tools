307455398c84817fe9381569230b01b0454174fc
Manuel Raimann <raimannma@outlook.de>
2024-10-26 10:15:31 +0200

    Update Leaderboard

--- migrations/07_create_leaderboard_view.sql
+++ migrations/07_create_leaderboard_view.sql
@@ -1,13 +1,15 @@
 DROP VIEW IF EXISTS leaderboard;
 CREATE MATERIALIZED VIEW leaderboard
-REFRESH EVERY 10 MINUTES ENGINE=MergeTree() ORDER BY(rank)
-POPULATE AS
-SELECT account_id,
-       ROUND(anyLast(player_score)) AS player_score,
+ REFRESH EVERY 10 MINUTES ENGINE=MergeTree() ORDER BY(rank)
+ POPULATE AS
+SELECT mh.account_id as account_id,
        anyLast(region_mode)  as region_mode,
+       row_number() OVER (ORDER BY ranked_badge_level DESC, player_score DESC) AS rank,
+       anyLast(pc.ranked_badge_level) as ranked_badge_level,
        COUNT()               AS matches_played,
-       row_number() OVER (ORDER BY player_score DESC) AS rank
-FROM mmr_history
-      INNER JOIN player_region ON player_region.account_id = account_id
-GROUP BY account_id
-SETTINGS allow_experimental_refreshable_materialized_view = 1;
+       ROUND(anyLast(player_score)) AS player_score
+FROM mmr_history mh
+      INNER JOIN player_region ON player_region.account_id = mh.account_id
+      LEFT JOIN player_card pc ON pc.account_id = mh.account_id
+GROUP BY mh.account_id
+ SETTINGS allow_experimental_refreshable_materialized_view = 1;
