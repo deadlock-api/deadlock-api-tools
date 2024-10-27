edab0e9c921e0a0d778c22622629b29c9e0e4f73
Manuel Raimann <raimannma@outlook.de>
2024-10-27 11:55:25 +0100

    Fix

--- matchdata-downloader/src/main.rs
+++ matchdata-downloader/src/main.rs
@@ -83,7 +83,7 @@ async fn main() {
 
     loop {
         println!("Fetching match ids to download");
-        let query = "SELECT DISTINCT match_id, cluster_id, metadata_salt, replay_salt FROM match_salts WHERE match_id NOT IN (SELECT match_id FROM match_info) AND NOT failed;";
+        let query = "SELECT DISTINCT match_id, cluster_id, metadata_salt, replay_salt FROM match_salts WHERE match_id NOT IN (SELECT match_id FROM match_info) AND NOT failed";
         let match_ids_to_fetch: Vec<MatchIdQueryResult> =
             client.query(query).fetch_all().await.unwrap();
         let match_ids_to_fetch: HashSet<MatchIdQueryResult> =
