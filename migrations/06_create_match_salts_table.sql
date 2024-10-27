e0b0c372906d0e456b46fa12a68ec16229e22037
Manuel Raimann <raimannma@outlook.de>
2024-10-27 11:44:10 +0100

    Track failed matches

--- migrations/06_create_match_salts_table.sql
+++ migrations/06_create_match_salts_table.sql
@@ -1,7 +1,8 @@
 CREATE TABLE IF NOT EXISTS match_salts
 (
     match_id UInt64,
-    cluster_id UInt32,
-    metadata_salt UInt32,
-    replay_salt UInt32
+    cluster_id Nullable(UInt32),
+    metadata_salt Nullable(UInt32),
+    replay_salt Nullable(UInt32),
+    failed Bool DEFAULT FALSE
 ) ENGINE = ReplacingMergeTree ORDER BY (match_id);
