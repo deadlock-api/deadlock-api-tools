45eedc3efe100af6b6bd0df8f84a0f8fdd9af6fd
Manuel Raimann <raimannma@outlook.de>
2024-11-01 01:28:06 +0100

    Add history fetcher

---
+++ history-fetcher/history_fetcher.py
@@ -0,0 +1,128 @@
+import os
+import time
+from concurrent.futures import ThreadPoolExecutor, as_completed
+from time import sleep
+
+from clickhouse_driver import Client
+from clickhouse_pool import ChPool
+from tqdm import tqdm
+from utils import PlayerMatchHistoryEntry, call_steam_proxy
+from valveprotos_py.citadel_gcmessages_client_pb2 import (
+    CMsgClientToGCGetMatchHistory,
+    CMsgClientToGCGetMatchHistoryResponse,
+    k_EMsgClientToGCGetMatchHistory,
+)
+
+UPDATE_INTERVAL = 10
+
+CH_POOL = ChPool(
+    host=os.getenv("CLICKHOUSE_HOST", "localhost"),
+    port=int(os.getenv("CLICKHOUSE_PORT", 9000)),
+    user=os.getenv("CLICKHOUSE_USER", "default"),
+    password=os.getenv("CLICKHOUSE_PASSWORD", ""),
+    database=os.getenv("CLICKHOUSE_DB", "default"),
+)
+
+
+def get_accounts(client: Client) -> list[int]:
+    query = f"""
+    SELECT DISTINCT account_id
+    FROM player
+    WHERE account_id NOT IN (SELECT account_id FROM player_match_history)
+    LIMIT 500;
+    """
+    accounts = [r[0] for r in client.execute(query)]
+    print(f"Found {len(accounts)} new accounts")
+    if len(accounts) < 500:
+        query = f"""
+        WITH last_cards AS (SELECT *
+                            FROM player_match_history
+                            ORDER BY account_id, created_at DESC
+                            LIMIT 1 BY account_id)
+        SELECT account_id
+        FROM last_cards
+        WHERE created_at < now() - INTERVAL 1 DAY
+        ORDER BY created_at
+        LIMIT %(limit)s;
+        """
+        accounts += [
+            r[0] for r in client.execute(query, {"limit": 500 - len(accounts)})
+        ]
+    return accounts
+
+
+def update_account(account_id: int) -> (int, list[PlayerMatchHistoryEntry]):
+    try:
+        msg = CMsgClientToGCGetMatchHistory()
+        msg.account_id = account_id
+        msg = call_steam_proxy(
+            k_EMsgClientToGCGetMatchHistory, msg, CMsgClientToGCGetMatchHistoryResponse
+        )
+        return account_id, [
+            PlayerMatchHistoryEntry.from_msg(match) for match in msg.matches
+        ]
+    except Exception as e:
+        print(f"Failed to update account {account_id}: {e}")
+        return account_id, None
+
+
+def main():
+    start = time.time()
+    with CH_POOL.get_client() as client:
+        account_ids = get_accounts(client)
+
+    if not account_ids:
+        print("No accounts to update")
+        sleep(5 * 60)
+        return
+
+    with ThreadPoolExecutor(max_workers=20) as pool:
+        futures = [pool.submit(update_account, a) for a in account_ids]
+        with CH_POOL.get_client() as client:
+            try:
+                match_histories = [
+                    p.result()
+                    for p in tqdm(as_completed(futures, timeout=60), total=len(futures))
+                ]
+            except TimeoutError:
+                print("TimeoutError")
+                return
+            client.execute(
+                f"INSERT INTO player_match_history (* EXCEPT(created_at)) VALUES",
+                [
+                    {
+                        "account_id": account_id,
+                        "match_id": e.match_id,
+                        "hero_id": e.hero_id,
+                        "hero_level": e.hero_level,
+                        "start_time": e.start_time,
+                        "game_mode": e.game_mode,
+                        "match_mode": e.match_mode,
+                        "player_team": e.player_team,
+                        "player_kills": e.player_kills,
+                        "player_deaths": e.player_deaths,
+                        "player_assists": e.player_assists,
+                        "denies": e.denies,
+                        "net_worth": e.net_worth,
+                        "last_hits": e.last_hits,
+                        "team_abandoned": e.team_abandoned,
+                        "abandoned_time_s": e.abandoned_time_s,
+                        "match_duration_s": e.match_duration_s,
+                        "match_result": e.match_result,
+                        "objectives_mask_team0": e.objectives_mask_team0,
+                        "objectives_mask_team1": e.objectives_mask_team1,
+                    }
+                    for account_id, match_history in match_histories
+                    for e in match_history or []
+                ],
+            )
+    end = time.time()
+    duration = end - start
+    print(f"Processed {len(account_ids)} accounts in {duration:.2f} seconds")
+    if duration < UPDATE_INTERVAL:
+        sleep(UPDATE_INTERVAL - (end - start))
+
+
+if __name__ == "__main__":
+    while True:
+        main()
---
+++ history-fetcher/utils.py
@@ -0,0 +1,91 @@
+import os
+from base64 import b64decode, b64encode
+from typing import TypeVar
+
+import requests
+from google.protobuf.message import Message
+from pydantic import BaseModel, ConfigDict
+from valveprotos_py.citadel_gcmessages_client_pb2 import (
+    CMsgClientToGCGetMatchHistoryResponse,
+)
+
+PROXY_URL = os.environ.get("PROXY_URL")
+PROXY_API_TOKEN = os.environ.get("PROXY_API_TOKEN")
+
+R = TypeVar("R", bound=Message)
+
+
+def call_steam_proxy(msg_type: int, msg: Message, response_type: type[R]) -> R:
+    data = call_steam_proxy_raw(msg_type, msg)
+    return response_type.FromString(data)
+
+
+def call_steam_proxy_raw(msg_type, msg):
+    msg_data = b64encode(msg.SerializeToString()).decode("utf-8")
+    body = {
+        "messageType": msg_type,
+        "timeoutMillis": 10_000,
+        "rateLimit": {
+            "messagePeriodMillis": 10,
+        },
+        "limitBufferingBehavior": "too_many_requests",
+        "data": msg_data,
+    }
+    response = requests.post(
+        PROXY_URL,
+        json=body,
+        headers={"Authorization": f"Bearer {PROXY_API_TOKEN}"},
+    )
+    response.raise_for_status()
+    data = response.json()["data"]
+    return b64decode(data)
+
+
+class PlayerMatchHistoryEntry(BaseModel):
+    model_config = ConfigDict(populate_by_name=True)
+
+    match_id: int
+    hero_id: int
+    hero_level: int
+    start_time: int
+    game_mode: int
+    match_mode: int
+    player_team: int
+    player_kills: int
+    player_deaths: int
+    player_assists: int
+    denies: int
+    net_worth: int
+    last_hits: int
+    team_abandoned: bool
+    abandoned_time_s: int
+    match_duration_s: int
+    match_result: int
+    objectives_mask_team0: int
+    objectives_mask_team1: int
+
+    @classmethod
+    def from_msg(
+        cls, msg: CMsgClientToGCGetMatchHistoryResponse.Match
+    ) -> "PlayerMatchHistoryEntry":
+        return cls(
+            abandoned_time_s=msg.abandoned_time_s,
+            denies=msg.denies,
+            game_mode=msg.game_mode,
+            hero_id=msg.hero_id,
+            hero_level=msg.hero_level,
+            last_hits=msg.last_hits,
+            match_duration_s=msg.match_duration_s,
+            match_id=msg.match_id,
+            match_mode=msg.match_mode,
+            match_result=msg.match_result,
+            net_worth=msg.net_worth,
+            objectives_mask_team0=msg.objectives_mask_team0,
+            objectives_mask_team1=msg.objectives_mask_team1,
+            player_assists=msg.player_assists,
+            player_deaths=msg.player_deaths,
+            player_kills=msg.player_kills,
+            player_team=msg.player_team,
+            start_time=msg.start_time,
+            team_abandoned=msg.team_abandoned,
+        )
