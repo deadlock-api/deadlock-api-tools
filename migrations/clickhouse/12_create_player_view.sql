fae0280ff3aa7f3f84b614bc58fcf22f38716133
Manuel Raimann <raimannma@outlook.de>
2024-11-04 18:35:41 +0100

    Fetch player region by searching for matches of mates

--- migrations/clickhouse/12_create_player_view.sql
+++ migrations/clickhouse/12_create_player_view.sql
@@ -3,10 +3,13 @@ CREATE MATERIALIZED VIEW player
 REFRESH EVERY 10 MINUTES
 ENGINE = MergeTree() ORDER BY account_id
 POPULATE AS
-SELECT DISTINCT players.account_id as account_id, region_mode
-FROM active_matches
-      ARRAY JOIN players
+SELECT DISTINCT players.account_id as account_id, region_mode FROM active_matches ARRAY JOIN players
 UNION DISTINCT
-SELECT DISTINCT account_id, NULL as region_mode
-FROM match_player
+WITH active_player_region AS (SELECT DISTINCT players.account_id as account_id, region_mode FROM active_matches ARRAY JOIN players)
+SELECT mp.account_id, apr.region_mode
+FROM match_player mp
+      LEFT JOIN match_player mates ON mp.match_id = mates.match_id
+      LEFT JOIN active_player_region apr ON apr.account_id = mates.account_id
+WHERE mp.account_id NOT IN (SELECT account_id FROM active_player_region)
+  AND mp.account_id != mates.account_id
  SETTINGS allow_experimental_refreshable_materialized_view = 1;
