5b7c9f6393248ad83ab58d7d55134d9bf4ecd753
Manuel Raimann <raimannma@outlook.de>
2024-11-13 16:00:06 +0100

    Remove account ordered leaderboard

--- migrations/clickhouse/07_create_leaderboard_view.sql
+++ migrations/clickhouse/07_create_leaderboard_view.sql
@@ -13,19 +13,3 @@ FROM mmr_history mh
       LEFT JOIN player_card pc ON pc.account_id = mh.account_id
 GROUP BY mh.account_id
  SETTINGS allow_experimental_refreshable_materialized_view = 1;
-
-DROP VIEW IF EXISTS leaderboard_account;
-CREATE MATERIALIZED VIEW leaderboard_account
- REFRESH EVERY 10 MINUTES ENGINE=MergeTree() ORDER BY(account_id)
- POPULATE AS
-SELECT mh.account_id as account_id,
-       anyLast(region_mode)  as region_mode,
-       row_number() OVER (ORDER BY ranked_badge_level DESC, player_score DESC) AS rank,
-       anyLast(pc.ranked_badge_level) as ranked_badge_level,
-       COUNT()               AS matches_played,
-       ROUND(anyLast(player_score)) AS player_score
-FROM mmr_history mh
-      INNER JOIN player ON player.account_id = mh.account_id
-      LEFT JOIN player_card pc ON pc.account_id = mh.account_id
-GROUP BY mh.account_id
- SETTINGS allow_experimental_refreshable_materialized_view = 1;
--- migrations/clickhouse/11_create_leaderboard_v2_view.sql
+++ migrations/clickhouse/11_create_leaderboard_v2_view.sql
@@ -16,22 +16,3 @@ FROM player_card pc
       LEFT JOIN player_hero_stats phs ON phs.account_id = pc.account_id
 GROUP BY pc.account_id
  SETTINGS allow_experimental_refreshable_materialized_view = 1;
-
-DROP VIEW IF EXISTS leaderboard_account_v2;
-CREATE MATERIALIZED VIEW leaderboard_account_v2
- REFRESH EVERY 10 MINUTES ENGINE=MergeTree() ORDER BY(account_id)
- POPULATE AS
-SELECT pc.account_id as account_id,
-       anyLast(region_mode) as region_mode,
-       rank() OVER (ORDER BY ranked_badge_level DESC) AS rank,
-       anyLast(pc.ranked_badge_level) as ranked_badge_level,
-       SUM(phs.wins) AS wins,
-       SUM(phs.matches) AS matches_played,
-       SUM(phs.kills) AS kills,
-       SUM(phs.deaths) AS deaths,
-       SUM(phs.assists) AS assists
-FROM player_card pc
-      INNER JOIN player ON player.account_id = pc.account_id
-      LEFT JOIN player_hero_stats phs ON phs.account_id = pc.account_id
-GROUP BY pc.account_id
- SETTINGS allow_experimental_refreshable_materialized_view = 1;
