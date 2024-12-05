2bd6a4e3bda377ca50d783b116a6a211ff7f453e
Manuel Raimann <raimannma@outlook.de>
2024-12-05 10:08:14 +0100

    Read leaderboard data from match history

--- migrations/clickhouse/08_create_player_card.sql
+++ migrations/clickhouse/08_create_player_card.sql
@@ -1,15 +1,12 @@
 DROP TABLE player_card;
-CREATE TABLE player_card
-(
- account_id         UInt32,
- ranked_badge_level UInt32,
- slots_slots_id Array(UInt32),
- slots_hero_id Array(UInt32),
- slots_hero_kills Array(UInt32),
- slots_hero_wins Array(UInt32),
- slots_stat_id Array(UInt32),
- slots_stat_score Array(UInt32),
-  created_at         DateTime DEFAULT now()
+CREATE TABLE player_card (
+    account_id UInt32,
+    ranked_badge_level UInt32,
+    slots_slots_id Array (UInt32),
+    slots_hero_id Array (UInt32),
+    slots_hero_kills Array (UInt32),
+    slots_hero_wins Array (UInt32),
+    slots_stat_id Array (UInt32),
+    slots_stat_score Array (UInt32),
+    created_at DateTime DEFAULT now()
 ) ENGINE = ReplacingMergeTree ORDER BY (account_id, created_at);
-
-DESCRIBE TABLE player_card FORMAT TSV;
--- migrations/clickhouse/11_create_leaderboard_v2_view.sql
+++ migrations/clickhouse/11_create_leaderboard_v2_view.sql
@@ -2,17 +2,22 @@ DROP VIEW IF EXISTS leaderboard_v2;
 CREATE MATERIALIZED VIEW leaderboard_v2
  REFRESH EVERY 10 MINUTES ENGINE=MergeTree() ORDER BY(rank)
  POPULATE AS
-SELECT pc.account_id as account_id,
-       anyLast(region_mode) as region_mode,
+WITH last_player_cards AS (SELECT account_id, ranked_badge_level
+                           FROM player_card
+                           WHERE ranked_badge_level > 0
+                           ORDER BY created_at DESC
+                           LIMIT 1 BY account_id)
+SELECT pmh.account_id                                 as account_id,
+       anyLast(region_mode)                           as region_mode,
        rank() OVER (ORDER BY ranked_badge_level DESC) AS rank,
-       anyLast(pc.ranked_badge_level) as ranked_badge_level,
-       SUM(phs.wins) AS wins,
-       SUM(phs.matches) AS matches_played,
-       SUM(phs.kills) AS kills,
-       SUM(phs.deaths) AS deaths,
-       SUM(phs.assists) AS assists
-FROM player_card pc
-      INNER JOIN player ON player.account_id = pc.account_id
-      LEFT JOIN player_hero_stats phs ON phs.account_id = pc.account_id
-GROUP BY pc.account_id
+       any(pc.ranked_badge_level)                     as ranked_badge_level,
+       SUM(pmh.match_result)                          AS wins,
+       COUNT(pmh.account_id)                          AS matches_played,
+       SUM(pmh.player_kills)                          AS kills,
+       SUM(pmh.player_deaths)                         AS deaths,
+       SUM(pmh.player_assists)                        AS assists
+FROM player_match_history pmh
+      INNER JOIN player ON player.account_id = pmh.account_id
+      INNER JOIN last_player_cards pc ON pmh.account_id = pc.account_id
+GROUP BY pmh.account_id
  SETTINGS allow_experimental_refreshable_materialized_view = 1;
