97496ac4d725b64d94859a8f5eece7a37caa0a69
Manuel Raimann <raimannma@outlook.de>
2024-12-20 12:42:41 +0100

    Set rate limit to 50/day

--- hltv-scraper/src/cmd/run_spectate_bot.rs
+++ hltv-scraper/src/cmd/run_spectate_bot.rs
@@ -299,8 +299,8 @@ impl SpectatorBot {
             .json(&serde_json::json!({
                 "message_kind": EgcCitadelClientMessages::KEMsgClientToGcSpectateLobby as u32,
                 "bot_in_all_groups": ["SpectateLobby"],
-                "rate_limit_cooldown_millis": 30 * 60 * 1000,
-                "job_cooldown_millis": 20 * 60 * 1000,
+                "rate_limit_cooldown_millis": 2 * 24 * 60 * 60 * 1000 / 50,
+                "job_cooldown_millis": 24 * 60 * 60 * 1000 / 50,
                 "data": BASE64_STANDARD.encode(data),
             }))
             .send()
@@ -343,8 +343,8 @@ impl SpectatorBot {
                         false
                     }
                     EResponse::KERateLimited => {
-                        warn!("[{label} {match_id}] Rate limited: {:?}, waiting 5m", &result);
-                        sleep(Duration::from_secs(5 * 60)).await;
+                        warn!("[{label} {match_id}] Rate limited: {:?}, waiting 10s", &result);
+                        sleep(Duration::from_secs(10)).await;
                         false
                     }
                     _ => {
