bbed90643c9030839fd5d75d56a3310e3b2798f6
Manuel Raimann <raimannma@outlook.de>
2025-01-28 16:28:18 +0100

    Reduce account processing to 90% of total accounts in history fetcher

--- history-fetcher/history_fetcher.py
+++ history-fetcher/history_fetcher.py
@@ -114,7 +114,7 @@ async def main(account_ids: list[int]):
 
 async def loop():
     chunk_size = 100
-    num_accounts = int(os.environ.get("NUM_ACCOUNTS", "100"))
+    num_accounts = int(os.environ.get("NUM_ACCOUNTS", "100")) * 0.9  # 90% of accounts
     while True:
         with CH_POOL.get_client() as client:
             account_ids = get_accounts(client)
