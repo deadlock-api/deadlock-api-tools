3245162b147487f85b5a513f703d002c28713b1f
Manuel Raimann <raimannma@outlook.de>
2025-02-11 20:36:42 +0100

    Replace Refreshable view with normal materialized view

--- migrations/clickhouse/01_create_finished_matches_view.sql
+++ migrations/clickhouse/01_create_finished_matches_view.sql
@@ -1,27 +1,157 @@
-DROP VIEW IF EXISTS finished_matches;
-CREATE MATERIALIZED VIEW finished_matches
-REFRESH EVERY 10 MINUTES
-    ENGINE = MergeTree() ORDER BY match_id
-    POPULATE
+DROP TABLE IF EXISTS finished_matches;
+
+CREATE TABLE finished_matches
+(
+    start_time DateTime,
+    winning_team UInt8,
+    match_id UInt64,
+    players Nested (
+        account_id UInt64,
+        team UInt8,
+        abandoned Bool,
+        hero_id UInt8
+    ),
+    lobby_id String,
+    net_worth_team_0 UInt32,
+    net_worth_team_1 UInt32,
+    duration_s UInt32,
+    spectators UInt32,
+    open_spectator_slots UInt32,
+    objectives_mask_team0 UInt16,
+    objectives_mask_team1 UInt16,
+    match_mode Enum8 (
+        'Invalid' = 0,
+        'Unranked' = 1,
+        'PrivateLobby' = 2,
+        'CoopBot' = 3,
+        'Ranked' = 4,
+        'ServerTest' = 5,
+        'Tutorial' = 6,
+        'HeroLabs' = 7
+    ),
+    game_mode Enum8 (
+        'Invalid' = 0, 'Normal' = 1, 'OneVsOneTest' = 2, 'Sandbox' = 3
+    ),
+    match_score UInt32,
+    region_mode Enum8 (
+        'Row' = 0,
+        'Europe' = 1,
+        'SEAsia' = 2,
+        'SAmerica' = 3,
+        'Russia' = 4,
+        'Oceania' = 5
+    ),
+    scraped_at DateTime64,
+    team0_core Bool,
+    team0_tier1_lane1 Bool,
+    team0_tier2_lane1 Bool,
+    team0_tier1_lane2 Bool,
+    team0_tier2_lane2 Bool,
+    team0_tier1_lane3 Bool,
+    team0_tier2_lane3 Bool,
+    team0_tier1_lane4 Bool,
+    team0_tier2_lane4 Bool,
+    team0_titan Bool,
+    team0_titan_shield_generator_1 Bool,
+    team0_titan_shield_generator_2 Bool,
+    team0_barrack_boss_lane1 Bool,
+    team0_barrack_boss_lane2 Bool,
+    team0_barrack_boss_lane3 Bool,
+    team0_barrack_boss_lane4 Bool,
+    team1_core Bool,
+    team1_tier1_lane1 Bool,
+    team1_tier2_lane1 Bool,
+    team1_tier1_lane2 Bool,
+    team1_tier2_lane2 Bool,
+    team1_tier1_lane3 Bool,
+    team1_tier2_lane3 Bool,
+    team1_tier1_lane4 Bool,
+    team1_tier2_lane4 Bool,
+    team1_titan Bool,
+    team1_titan_shield_generator_1 Bool,
+    team1_titan_shield_generator_2 Bool,
+    team1_barrack_boss_lane1 Bool,
+    team1_barrack_boss_lane2 Bool,
+    team1_barrack_boss_lane3 Bool,
+    team1_barrack_boss_lane4 Bool,
+    winner UInt8,
+    sign Int8
+)
+ENGINE = VersionedCollapsingMergeTree(sign, scraped_at)
+ORDER BY match_id;
+
+DROP VIEW IF EXISTS finished_matches_mv;
+CREATE MATERIALIZED VIEW finished_matches_mv TO finished_matches
 AS
-SELECT *,
-       CASE
-        WHEN am.team0_core AND NOT am.team1_core THEN 0
-        WHEN am.team1_core AND NOT am.team0_core THEN 1
-        WHEN am.team0_titan AND NOT am.team1_titan THEN 0
-        WHEN am.team1_titan AND NOT am.team0_titan THEN 1
-        WHEN am.team0_titan_shield_generator_1 + am.team0_titan_shield_generator_2 >
-             am.team1_titan_shield_generator_1 + am.team1_titan_shield_generator_2 + 1 THEN 0
-        WHEN am.team1_titan_shield_generator_1 + am.team1_titan_shield_generator_2 >
-             am.team0_titan_shield_generator_1 + am.team0_titan_shield_generator_2 + 1 THEN 1
-        WHEN am.net_worth_team_0 > am.net_worth_team_1 + 15000
-         THEN 0
-        WHEN am.net_worth_team_1 > am.net_worth_team_0 + 15000
-         THEN 1
-        END
-        AS winner
-FROM active_matches am
-WHERE winner IS NOT NULL
-ORDER BY match_id, scraped_at DESC
-LIMIT 1 BY match_id
-SETTINGS asterisk_include_alias_columns = 1, allow_experimental_refreshable_materialized_view = 1;
+SELECT
+    start_time,
+    winning_team,
+    match_id,
+    players.hero_id AS `players.hero_id`,
+    players.account_id AS `players.account_id`,
+    players.team AS `players.team`,
+    `players.abandoned` AS `players.abandoned`,
+    lobby_id,
+    net_worth_team_0,
+    net_worth_team_1,
+    duration_s,
+    spectators,
+    open_spectator_slots,
+    objectives_mask_team0,
+    objectives_mask_team1,
+    match_mode,
+    game_mode,
+    match_score,
+    region_mode,
+    scraped_at,
+    team0_core,
+    team0_tier1_lane1,
+    team0_tier2_lane1,
+    team0_tier1_lane2,
+    team0_tier2_lane2,
+    team0_tier1_lane3,
+    team0_tier2_lane3,
+    team0_tier1_lane4,
+    team0_tier2_lane4,
+    team0_titan,
+    team0_titan_shield_generator_1,
+    team0_titan_shield_generator_2,
+    team0_barrack_boss_lane1,
+    team0_barrack_boss_lane2,
+    team0_barrack_boss_lane3,
+    team0_barrack_boss_lane4,
+    team1_core,
+    team1_tier1_lane1,
+    team1_tier2_lane1,
+    team1_tier1_lane2,
+    team1_tier2_lane2,
+    team1_tier1_lane3,
+    team1_tier2_lane3,
+    team1_tier1_lane4,
+    team1_tier2_lane4,
+    team1_titan,
+    team1_titan_shield_generator_1,
+    team1_titan_shield_generator_2,
+    team1_barrack_boss_lane1,
+    team1_barrack_boss_lane2,
+    team1_barrack_boss_lane3,
+    team1_barrack_boss_lane4,
+    1 AS sign,
+    CASE
+        WHEN team0_core AND NOT team1_core THEN 0
+        WHEN team1_core AND NOT team0_core THEN 1
+        WHEN team0_titan AND NOT team1_titan THEN 0
+        WHEN team1_titan AND NOT team0_titan THEN 1
+        WHEN
+            team0_titan_shield_generator_1 + team0_titan_shield_generator_2
+            > team1_titan_shield_generator_1 + team1_titan_shield_generator_2 + 1 THEN 0
+        WHEN
+            team1_titan_shield_generator_1 + team1_titan_shield_generator_2
+            > team0_titan_shield_generator_1 + team0_titan_shield_generator_2 + 1 THEN 1
+        WHEN net_worth_team_0 > net_worth_team_1 + 15000
+            THEN 0
+        WHEN net_worth_team_1 > net_worth_team_0 + 15000
+            THEN 1
+    END AS winner
+FROM active_matches
+WHERE winner IS NOT NULL;
--- migrations/clickhouse/18_create_match_player_encoded_items_view.sql
+++ migrations/clickhouse/18_create_match_player_encoded_items_view.sql
@@ -1,28 +1,56 @@
-DROP VIEW IF EXISTS match_player_encoded_items;
+DROP TABLE IF EXISTS match_player_encoded_items;
+DROP VIEW IF EXISTS match_player_encoded_items_mv;
 
-CREATE MATERIALIZED VIEW match_player_encoded_items
- REFRESH EVERY 30 MINUTES
- ENGINE = MergeTree()
- ORDER BY (hero_id)
- PARTITION BY hero_id
- POPULATE
+CREATE TABLE match_player_encoded_items
+(
+ match_id      UInt64,
+ account_id    UInt32,
+ hero_id       UInt32,
+ average_badge Nullable(UInt32),
+ encoded_items Array(BOOL),
+ won           Bool
+)
+ ENGINE = ReplacingMergeTree()
+  ORDER BY (match_id, account_id)
+  PARTITION BY hero_id;
+
+CREATE MATERIALIZED VIEW match_player_encoded_items_mv TO match_player_encoded_items
 AS
-WITH all_items AS (SELECT groupUniqArray(item_id) as items_arr FROM items)
+WITH all_items AS (SELECT groupUniqArray(item_id) AS items_arr FROM items)
 SELECT match_id,
        account_id,
        hero_id,
-       team == 'Team0' ? mi.average_badge_team0 : mi.average_badge_team1 as average_badge,
-       arrayMap(x -> toBool(has(items.item_id, x)), items_arr)        as encoded_items,
-       won
-FROM match_player FINAL
-      INNER JOIN match_info mi FINAL USING (match_id)
+       won ? True : False                                                 as won,
+       if(team = 'Team0', mi.average_badge_team0, mi.average_badge_team1) AS average_badge,
+       arrayMap(
+        x -> toBool(has(items.item_id, x)),
+        items_arr
+       )                                                                  AS encoded_items
+FROM match_player
+      INNER JOIN match_info AS mi USING (match_id)
       INNER JOIN all_items ON 1 = 1
-WHERE mi.average_badge_team0 is not null
-  and mi.average_badge_team1 is not null
-  and team IN ('Team0', 'Team1')
-  and game_mode = 'Normal'
-  and match_id >= 28626954
-  and match_outcome = 'TeamWin'
-  and match_mode IN ('Ranked', 'Unranked')
-ORDER BY hero_id, average_badge
- SETTINGS allow_experimental_refreshable_materialized_view = 1;
+WHERE mi.average_badge_team0 IS NOT null
+  AND mi.average_badge_team1 IS NOT null
+  AND team IN ('Team0', 'Team1')
+  AND game_mode = 'Normal'
+  AND match_outcome = 'TeamWin'
+  AND match_mode IN ('Ranked', 'Unranked');
+
+INSERT INTO match_player_encoded_items_mv
+WITH all_items AS (SELECT groupUniqArray(item_id) AS items_arr FROM items)
+SELECT match_id,
+       account_id,
+       hero_id,
+       won ? True : False                                                 as won,
+       if(team = 'Team0', mi.average_badge_team0, mi.average_badge_team1) AS average_badge,
+       arrayMap(x -> toBool(has(items.item_id, x)), items_arr)            AS encoded_items
+FROM match_player
+      INNER JOIN match_info AS mi USING (match_id)
+      INNER JOIN all_items ON 1 = 1
+ PREWHERE match_id >= 31247321
+WHERE mi.average_badge_team0 IS NOT null
+  AND mi.average_badge_team1 IS NOT null
+  AND team IN ('Team0', 'Team1')
+  AND game_mode = 'Normal'
+  AND match_outcome = 'TeamWin'
+  AND match_mode IN ('Ranked', 'Unranked');
