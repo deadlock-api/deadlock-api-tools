4f815ca8ed66208d56e038128f695fa5af26dd63
Manuel Raimann <raimannma@outlook.de>
2025-03-23 22:02:32 +0100

    Fetch histories that we missed

--- history-fetcher/src/main.rs
+++ history-fetcher/src/main.rs
@@ -2,15 +2,24 @@ mod types;
 
 use crate::types::{PlayerMatchHistory, PlayerMatchHistoryEntry};
 use arl::RateLimiter;
+use clickhouse::Row;
+use itertools::Itertools;
 use metrics::{counter, gauge};
 use rand::prelude::SliceRandom;
 use rand::rng;
+use serde::Deserialize;
 use std::time::Duration;
 use tracing::{debug, error, info, instrument};
 use valveprotos::deadlock::c_msg_client_to_gc_get_match_history_response::EResult;
 use valveprotos::deadlock::{
     CMsgClientToGcGetMatchHistory, CMsgClientToGcGetMatchHistoryResponse, EgcCitadelClientMessages,
 };
+
+#[derive(Debug, Row, Deserialize)]
+struct Account {
+    id: u64,
+    max_match_id: Option<u64>,
+}
 
 #[tokio::main]
 async fn main() -> anyhow::Result<()> {
@@ -20,7 +29,7 @@ async fn main() -> anyhow::Result<()> {
     let http_client = reqwest::Client::new();
     let ch_client = common::get_ch_client()?;
 
-    let limiter = RateLimiter::new(20, Duration::from_secs(60));
+    let limiter = RateLimiter::new(25, Duration::from_secs(60));
 
     loop {
         let mut accounts = match fetch_accounts(&ch_client).await {
@@ -39,9 +48,14 @@ async fn main() -> anyhow::Result<()> {
             }
         };
         accounts.shuffle(&mut rng());
-        futures::future::join_all(accounts.iter().map(|account_id| {
-            update_account_limited(&limiter, &ch_client, &http_client, *account_id)
-        }))
+        futures::future::join_all(
+            accounts
+                .iter()
+                .sorted_by_key(|a| a.max_match_id.unwrap_or_default())
+                .rev()
+                .take(10000)
+                .map(|account| update_account_limited(&limiter, &ch_client, &http_client, account)),
+        )
         .await;
     }
 }
@@ -50,15 +64,15 @@ async fn main() -> anyhow::Result<()> {
 async fn update_account(
     ch_client: &clickhouse::Client,
     http_client: &reqwest::Client,
-    account_id: u32,
+    account: &Account,
 ) {
-    let match_history = match fetch_account_match_history(http_client, account_id).await {
+    let match_history = match fetch_account_match_history(http_client, account).await {
         Ok((_, match_history)) => match_history,
         Err(e) => {
             counter!("history_fetcher.fetch_match_history.failure").increment(1);
             error!(
                 "Failed to fetch match history for account {}, error: {:?}, skipping",
-                account_id, e
+                account.id, e
             );
             return;
         }
@@ -71,18 +85,18 @@ async fn update_account(
         counter!("history_fetcher.fetch_match_history.failure").increment(1);
         error!(
             "Failed to fetch match history for account {}, result: {:?}, skipping",
-            account_id, match_history.result
+            account.id, match_history.result
         );
         return;
     }
     let match_history = match_history.matches;
     if match_history.is_empty() {
-        debug!("No new matches for account {}", account_id);
+        debug!("No new matches for account {}", account.id);
         return;
     }
     let match_history: PlayerMatchHistory = match_history
         .into_iter()
-        .filter_map(|r| PlayerMatchHistoryEntry::from_protobuf(account_id, r))
+        .filter_map(|r| PlayerMatchHistoryEntry::from_protobuf(account.id as u32, r))
         .collect();
     match insert_match_history(ch_client, &match_history).await {
         Ok(_) => {
@@ -90,33 +104,47 @@ async fn update_account(
             info!(
                 "Inserted {} new matches for account {}",
                 match_history.len(),
-                account_id
+                account.id
             )
         }
         Err(e) => {
             counter!("history_fetcher.insert_match_history.failure").increment(1);
             error!(
                 "Failed to insert match history for account {}: {:?}",
-                account_id, e
+                account.id, e
             )
         }
     }
 }
 
-async fn fetch_accounts(ch_client: &clickhouse::Client) -> clickhouse::error::Result<Vec<u32>> {
+async fn fetch_accounts(ch_client: &clickhouse::Client) -> clickhouse::error::Result<Vec<Account>> {
     ch_client
         .query(
             r#"
-    SELECT DISTINCT account_id
+    WITH matches AS (
+            SELECT match_id
+            FROM match_info
+            WHERE start_time BETWEEN now() - INTERVAL 6 MONTH AND now() - INTERVAL 2 WEEK),
+        histories AS (
+            SELECT match_id, account_id
+            FROM player_match_history
+            WHERE match_id IN matches)
+    SELECT account_id as id, max(match_id) as max_match_id
+    FROM match_player
+    WHERE match_id IN matches AND (match_id, account_id) NOT IN histories
+    GROUP BY account_id
+
+    UNION DISTINCT
+
+    SELECT account_id as id, NULL as max_match_id
     FROM player
     WHERE account_id NOT IN (SELECT account_id FROM player_match_history)
 
     UNION DISTINCT
 
-    SELECT DISTINCT account_id
+    SELECT account_id as id, NULL as max_match_id
     FROM match_player
-    INNER JOIN match_info mi USING (match_id)
-    WHERE mi.start_time > now() - INTERVAL 2 WEEK
+    WHERE match_id IN (SELECT match_id FROM match_info WHERE start_time > now() - INTERVAL 2 WEEK)
     "#,
         )
         .fetch_all()
@@ -125,10 +153,11 @@ async fn fetch_accounts(ch_client: &clickhouse::Client) -> clickhouse::error::Re
 
 async fn fetch_account_match_history(
     http_client: &reqwest::Client,
-    account_id: u32,
+    account: &Account,
 ) -> reqwest::Result<(String, CMsgClientToGcGetMatchHistoryResponse)> {
     let msg = CMsgClientToGcGetMatchHistory {
-        account_id: account_id.into(),
+        account_id: (account.id as u32).into(),
+        continue_cursor: account.max_match_id.map(|a| a + 1),
         ..Default::default()
     };
     common::call_steam_proxy(
@@ -158,11 +187,11 @@ async fn update_account_limited(
     limiter: &RateLimiter,
     ch_client: &clickhouse::Client,
     http_client: &reqwest::Client,
-    account_id: u32,
+    account: &Account,
 ) {
-    if account_id == 0 {
+    if account.id == 0 {
         return;
     }
     limiter.wait().await;
-    update_account(ch_client, http_client, account_id).await
+    update_account(ch_client, http_client, account).await
 }
