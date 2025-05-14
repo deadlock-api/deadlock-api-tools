bfa46207798b266879cbdd9c41f6c8191aba9627
Manuel Raimann <raimannma@outlook.de>
2025-05-14 16:02:21 +0200

    Simplify query to remove unnecessary CTE.
    Refactored the SQL query by eliminating the redundant CTE and directly querying the `player_match_history` table. This improves readability and potentially enhances query performance. The logic and filtering remain unchanged.

--- salt-scraper/src/main.rs
+++ salt-scraper/src/main.rs
@@ -39,12 +39,11 @@ async fn main() -> anyhow::Result<()> {
     loop {
         // let query = "SELECT DISTINCT match_id FROM finished_matches WHERE start_time < now() - INTERVAL '3 hours' AND match_id NOT IN (SELECT match_id FROM match_salts UNION DISTINCT SELECT match_id FROM match_info) ORDER BY start_time DESC LIMIT 1000";
         let query = r"
-        WITH matches AS (
-            SELECT match_id FROM player_match_history WHERE match_mode IN ('Ranked', 'Unranked') AND toDateTime(start_time) BETWEEN '2024-12-01' AND now() - INTERVAL '2 hours'
-        )
         SELECT DISTINCT match_id
-        FROM matches
-        WHERE match_id NOT IN (SELECT match_id FROM match_salts)
+        FROM player_match_history
+        WHERE match_mode IN ('Ranked', 'Unranked')
+            AND start_time BETWEEN '2024-12-01' AND now() - INTERVAL 2 HOUR
+            AND match_id NOT IN (SELECT match_id FROM match_salts)
             AND match_id NOT IN (SELECT match_id FROM match_info)
         ORDER BY match_id DESC
         LIMIT 100
