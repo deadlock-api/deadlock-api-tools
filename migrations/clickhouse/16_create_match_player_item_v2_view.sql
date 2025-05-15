ebd20d6be199c53c38ac050688ddef77eda93411
Manuel Raimann <raimannma@outlook.de>
2025-05-15 21:20:14 +0200

    Add new columns for item buy/sell time and match duration
    This commit introduces `buy_time_s`, `sold_time_s`, and `match_duration_s` columns to the materialized view and insert statements. These changes enhance data granularity for tracking item lifecycle and match duration. Updated JOIN logic and null-handling ensure consistency and data quality.

--- migrations/clickhouse/16_create_match_player_item_v2_view.sql
+++ migrations/clickhouse/16_create_match_player_item_v2_view.sql
@@ -10,6 +10,9 @@ CREATE TABLE match_player_item_v2
  `item_id`             UInt32,
  `won`                 Bool,
  `average_match_badge` UInt64,
+ buy_time_s            UInt32,
+ sold_time_s           Nullable(UInt32),
+ match_duration_s      UInt32,
 
  INDEX idx_match_id match_id TYPE minmax
 )
@@ -20,30 +23,42 @@ CREATE TABLE match_player_item_v2
 CREATE MATERIALIZED VIEW match_player_item_v2_mv
  TO match_player_item_v2
 AS
-SELECT mi.start_time      AS start_time,
-       mp.match_id        AS match_id,
-       mp.account_id      AS account_id,
-       mp.hero_id         AS hero_id,
-       items.item_id      AS item_id,
-       won ? TRUE : FALSE as won,
-       coalesce(intDivOrZero(mi.average_badge_team0 + mi.average_badge_team1, 2), 0) AS average_match_badge
+SELECT mi.start_time                                                                 AS start_time,
+       mp.match_id                                                                   AS match_id,
+       mp.account_id                                                                 AS account_id,
+       mp.hero_id                                                                    AS hero_id,
+       items.item_id                                                                 AS item_id,
+       won ? TRUE : FALSE                                                            as won,
+       coalesce(intDivOrZero(mi.average_badge_team0 + mi.average_badge_team1, 2), 0) AS average_match_badge,
+       items.game_time_s                                                             AS buy_time_s,
+       nullIf(items.sold_time_s, 0)                                                  AS sold_time_s,
+       mi.duration_s                                                                 AS match_duration_s
 FROM match_player AS mp
-      INNER ANY JOIN match_info AS mi USING (match_id)
-      ARRAY JOIN items.item_id
+      INNER ANY
+      JOIN match_info AS mi USING (match_id)
+      ARRAY JOIN items
 WHERE won IS NOT NULL
-  AND match_outcome = 'TeamWin' AND match_mode IN ('Ranked', 'Unranked') AND game_mode = 'Normal';
+  AND match_outcome = 'TeamWin'
+  AND match_mode IN ('Ranked', 'Unranked')
+  AND game_mode = 'Normal';
 
 INSERT INTO match_player_item_v2
-SELECT mi.start_time      AS start_time,
-       mp.match_id        AS match_id,
-       mp.account_id      AS account_id,
-       mp.hero_id         AS hero_id,
-       items.item_id      AS item_id,
-       won ? TRUE : FALSE as won,
-       coalesce(intDivOrZero(mi.average_badge_team0 + mi.average_badge_team1, 2), 0) AS average_match_badge
-FROM match_player AS mp
-      INNER ANY JOIN match_info AS mi USING (match_id)
-      ARRAY JOIN items.item_id
+SELECT mi.start_time                                                                 AS start_time,
+       mp.match_id                                                                   AS match_id,
+       mp.account_id                                                                 AS account_id,
+       mp.hero_id                                                                    AS hero_id,
+       items.item_id                                                                 AS item_id,
+       won ? TRUE : FALSE                                                            as won,
+       coalesce(intDivOrZero(mi.average_badge_team0 + mi.average_badge_team1, 2), 0) AS average_match_badge,
+       items.game_time_s                                                             AS buy_time_s,
+       nullIf(items.sold_time_s, 0)                                                  AS sold_time_s,
+       mi.duration_s                                                                 AS match_duration_s
+FROM match_player AS mp FINAL
+      INNER ANY
+      JOIN match_info AS mi FINAL USING (match_id)
+      ARRAY JOIN items
 WHERE won IS NOT NULL
-  AND match_outcome = 'TeamWin' AND match_mode IN ('Ranked', 'Unranked') AND game_mode = 'Normal'
+  AND match_outcome = 'TeamWin'
+  AND match_mode IN ('Ranked', 'Unranked')
+  AND game_mode = 'Normal'
  SETTINGS max_partitions_per_insert_block = 10000;
