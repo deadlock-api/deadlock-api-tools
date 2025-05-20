6c32b31f41e84fc38d123e1fe4d59acb7f792160
Manuel Raimann <raimannma@outlook.de>
2025-05-20 12:11:46 +0200

    Revert "Enable feature-based dependency configuration across services"
    This reverts commit fce39a5f0aa42c8170dd9c98530691e8ae522926.

--- common/src/clients.rs
+++ common/src/clients.rs
@@ -1,24 +1,17 @@
-#[cfg(feature = "clickhouse")]
 use clickhouse::Compression;
-#[cfg(feature = "redis")]
 use fred::clients::Client as RedisClient;
-#[cfg(feature = "redis")]
 use fred::interfaces::{ClientLike, FredResult};
-#[cfg(feature = "redis")]
 use fred::prelude::{Config as RedisConfig, ReconnectPolicy};
-#[cfg(feature = "object-store")]
 use object_store::ClientOptions;
-#[cfg(feature = "object-store")]
 use object_store::aws::AmazonS3Builder;
-#[cfg(feature = "postgres")]
 use sqlx::postgres::{PgConnectOptions, PgPoolOptions};
-#[cfg(feature = "postgres")]
-use sqlx::{Pool, Postgres};
+use sqlx::{ConnectOptions, Pool, Postgres};
 use std::env;
+use std::env::VarError;
 use std::time::Duration;
+use tracing::log::LevelFilter;
 
-#[cfg(feature = "clickhouse")]
-pub fn get_ch_client() -> Result<clickhouse::Client, env::VarError> {
+pub fn get_ch_client() -> Result<clickhouse::Client, VarError> {
     Ok(clickhouse::Client::default()
         .with_url(env::var("CLICKHOUSE_URL").unwrap_or("http://127.0.0.1:8123".to_string()))
         .with_user(env::var("CLICKHOUSE_USER")?)
@@ -27,7 +20,6 @@ pub fn get_ch_client() -> Result<clickhouse::Client, env::VarError> {
         .with_compression(Compression::None))
 }
 
-#[cfg(feature = "object-store")]
 pub fn get_store() -> anyhow::Result<impl object_store::ObjectStore> {
     Ok(AmazonS3Builder::new()
         .with_region(env::var("S3_REGION")?)
@@ -40,7 +32,6 @@ pub fn get_store() -> anyhow::Result<impl object_store::ObjectStore> {
         .build()?)
 }
 
-#[cfg(feature = "object-store")]
 pub fn get_cache_store() -> anyhow::Result<impl object_store::ObjectStore> {
     Ok(AmazonS3Builder::new()
         .with_region(env::var("S3_CACHE_REGION")?)
@@ -53,20 +44,19 @@ pub fn get_cache_store() -> anyhow::Result<impl object_store::ObjectStore> {
         .build()?)
 }
 
-#[cfg(feature = "postgres")]
 pub async fn get_pg_client() -> anyhow::Result<Pool<Postgres>> {
     let pg_options = PgConnectOptions::new_without_pgpass()
         .host(&env::var("POSTGRES_HOST").unwrap_or("localhost".to_string()))
         .username(&env::var("POSTGRES_USERNAME").unwrap_or("postgres".to_string()))
         .password(&env::var("POSTGRES_PASSWORD")?)
-        .database(&env::var("POSTGRES_DBNAME").unwrap_or("postgres".to_string()));
+        .database(&env::var("POSTGRES_DBNAME").unwrap_or("postgres".to_string()))
+        .log_slow_statements(LevelFilter::Warn, Duration::from_secs(5));
     Ok(PgPoolOptions::new()
         .max_connections(10)
         .connect_with(pg_options)
         .await?)
 }
 
-#[cfg(feature = "redis")]
 pub async fn get_redis_client() -> FredResult<RedisClient> {
     let config =
         RedisConfig::from_url(&env::var("REDIS_URL").unwrap_or("redis://127.0.0.1".to_string()))?;
--- common/src/lib.rs
+++ common/src/lib.rs
@@ -2,12 +2,10 @@ mod assets;
 mod clients;
 mod steam;
 mod telemetry;
-#[cfg(feature = "steam-proxy")]
 mod utils;
 
 pub use assets::*;
 pub use clients::*;
 pub use steam::*;
 pub use telemetry::*;
-#[cfg(feature = "steam-proxy")]
 pub use utils::*;
