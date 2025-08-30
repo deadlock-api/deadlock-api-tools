a4c8eea2f3310ba1506010972d92acf13fc79ef5
Manuel Raimann <raimannma@outlook.de>
2025-08-30 12:43:28 +0200

    chore: Change bot_id type from INTEGER to VARCHAR(32) in bot_friends table

--- migrations/postgres/06_create_bot_friends_table.sql
+++ migrations/postgres/06_create_bot_friends_table.sql
@@ -2,7 +2,7 @@ DROP TABLE IF EXISTS bot_friends;
 
 CREATE TABLE bot_friends
 (
- bot_id     INTEGER PRIMARY KEY,
+ bot_id     VARCHAR(32) PRIMARY KEY,
  friend_id  INTEGER,
  created_at TIMESTAMP DEFAULT now()
 );
