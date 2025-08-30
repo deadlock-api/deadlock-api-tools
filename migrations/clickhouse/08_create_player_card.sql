71a4abe18e1d8f04a6d07d55e371b39112e0eb3c
Manuel Raimann <raimannma@outlook.de>
2025-08-30 12:43:38 +0200

    chore: Update player_card table creation to use IF EXISTS and IF NOT EXISTS, and make fields nullable

--- migrations/clickhouse/08_create_player_card.sql
+++ migrations/clickhouse/08_create_player_card.sql
@@ -1,12 +1,13 @@
-DROP TABLE player_card;
-CREATE TABLE player_card (
-    account_id UInt32,
-    ranked_badge_level UInt32,
-    slots_slots_id Array (UInt32),
-    slots_hero_id Array (UInt32),
-    slots_hero_kills Array (UInt32),
-    slots_hero_wins Array (UInt32),
-    slots_stat_id Array (UInt32),
-    slots_stat_score Array (UInt32),
-    created_at DateTime DEFAULT now()
+DROP TABLE IF EXISTS player_card;
+CREATE TABLE IF NOT EXISTS player_card
+(
+ account_id         UInt32,
+ ranked_badge_level Nullable(UInt32),
+ slots_slots_id     Array(Nullable(UInt32)),
+ slots_hero_id      Array(Nullable(UInt32)),
+ slots_hero_kills   Array(Nullable(UInt32)),
+ slots_hero_wins    Array(Nullable(UInt32)),
+ slots_stat_id      Array(Nullable(Int32)),
+ slots_stat_score   Array(Nullable(UInt32)),
+ created_at         DateTime DEFAULT now()
 ) ENGINE = ReplacingMergeTree ORDER BY (account_id, created_at);
