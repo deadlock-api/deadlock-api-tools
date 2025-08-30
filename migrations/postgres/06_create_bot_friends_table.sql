7d207b77399e3e40a0e6e20296594f0117be3a6f
Manuel Raimann <raimannma@outlook.de>
2025-08-30 17:50:50 +0200

    chore: Modify bot_friends table creation to use IF NOT EXISTS and update primary key constraint

--- migrations/postgres/06_create_bot_friends_table.sql
+++ migrations/postgres/06_create_bot_friends_table.sql
@@ -1,8 +1,9 @@
 DROP TABLE IF EXISTS bot_friends;
 
-CREATE TABLE bot_friends
+CREATE TABLE IF NOT EXISTS bot_friends
 (
- bot_id     VARCHAR(32) PRIMARY KEY,
+ bot_id     VARCHAR(32),
  friend_id  INTEGER,
- created_at TIMESTAMP DEFAULT now()
+ created_at TIMESTAMP DEFAULT now(),
+ PRIMARY KEY (bot_id, friend_id)
 );
