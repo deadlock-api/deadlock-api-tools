5c73fa7a7daa970dfe14012022e85a85acd41a85
Manuel Raimann <raimannma@outlook.de>
2025-08-30 17:53:33 +0200

    chore: Update bot_friends table to set friend_id as primary key and remove composite key

--- migrations/postgres/06_create_bot_friends_table.sql
+++ migrations/postgres/06_create_bot_friends_table.sql
@@ -2,8 +2,7 @@ DROP TABLE IF EXISTS bot_friends;
 
 CREATE TABLE IF NOT EXISTS bot_friends
 (
+ friend_id  INTEGER PRIMARY KEY,
  bot_id     VARCHAR(32),
- friend_id  INTEGER,
- created_at TIMESTAMP DEFAULT now(),
- PRIMARY KEY (bot_id, friend_id)
+ created_at TIMESTAMP DEFAULT now()
 );
