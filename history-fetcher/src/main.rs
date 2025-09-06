75b49cd5ba0772b673a03f391b08f59984c79e96
Manuel Raimann <raimannma@outlook.de>
2025-09-06 13:16:06 +0200

    chore: Update request rate limit to 50 requests per day for improved performance

--- history-fetcher/src/main.rs
+++ history-fetcher/src/main.rs
@@ -165,7 +165,7 @@ async fn fetch_account_match_history(
         &msg,
         Some(&["GetMatchHistory"]),
         None,
-        Duration::from_secs(24 * 60 * 60 / 100), // 100 requests per day
+        Duration::from_secs(24 * 60 * 60 / 50), // 50 requests per day
         Duration::from_secs(5),
     )
     .await
