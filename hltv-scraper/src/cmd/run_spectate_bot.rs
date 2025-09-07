614e896c47140f1abf2970570469fa00c8042d2f
Manuel Raimann <raimannma@outlook.de>
2025-09-08 00:17:46 +0200

    chore: Adjust cooldown parameters for improved bot performance in spectating

--- hltv-scraper/src/cmd/run_spectate_bot.rs
+++ hltv-scraper/src/cmd/run_spectate_bot.rs
@@ -285,8 +285,8 @@ impl SpectatorBot {
             .json(&serde_json::json!({
                 "message_kind": EgcCitadelClientMessages::KEMsgClientToGcSpectateLobby as u32,
                 "bot_in_all_groups": ["SpectateLobby"],
-                "rate_limit_cooldown_millis": 2 * 24 * 60 * 60 * 1000 / 25,
-                "job_cooldown_millis": 24 * 60 * 60 * 1000 / 25,
+                "rate_limit_cooldown_millis": 2 * 24 * 60 * 60 * 1000 / 50,
+                "job_cooldown_millis": 24 * 60 * 60 * 1000 / 50,
                 "soft_cooldown_millis": 5 * 60 * 1000,
                 "data": BASE64_STANDARD.encode(data),
             }))
